dc922d3cb0fb81b60728fd8808ede19f1e5a7343
Saurav Kumar yadav <sauravkumaryadav100@gmail.com>
2021-09-01 22:46:01 +0545

    Tri-diagnol Matrix

--- DSA/Assignment 1A/1b.c
+++ DSA/Assignment 1A/1b.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main()
 {
     int num;
     printf("Enter number of rows");
     scanf("%d", &num);
-    int array[num];
+    int k = 0;
+    int array[3 * num + 2];
     for (int i = 0; i < num; i++)
     {
         for (int j = 0; j < num; j++)
         {
-            if (i == j)
+            if (abs(i - j) <= 1)
             {
-                printf("Enter the element of %d row  %d column \n", i + 1, j + 1);
-                scanf("%d", &array[i]);
+                printf("Enter the element of %d row and %d colomn\n", i + 1, j + 1);
+                scanf("%d", &array[k]);
+                k++;
             }
         }
     }
+    k = 0;
     for (int i = 0; i < num; i++)
     {
         for (int j = 0; j < num; j++)
         {
-            if (i == j)
+            if (abs(i - j) <= 1)
             {
-                printf("%d\t", array[i]);
+                printf("%d \t", array[k]);
+                k++;
             }
             else
                 printf("0\t");
